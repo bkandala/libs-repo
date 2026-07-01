7b5de382ae57124a1e74ce04a0e6c73a37d8d60b
bkandala <bkandala@users.noreply.github.com>
2026-07-01 16:43:16 -0700

    Remove unused loadDa function for dapreview

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,8 +11,3 @@ loadPage({
   libsUrl: LIBS,
   codeRoot: '/',
 });
-(async function loadDa() {
-  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
-  // eslint-disable-next-line import/no-unresolved
-  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
-}());
